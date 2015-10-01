9bddafd6d77a3661c35f85366e1297bf2ce43612
andrewf <andrewf@porch.com>
2015-10-01 15:31:57 -0700

    validate time input format. convert time string to ms for storage and back again for display.

--- lap-times/lap-times.js
+++ lap-times/lap-times.js
@@ -2,20 +2,23 @@ LeaderBoards = new Mongo.Collection("leaderboards");
 LapTimes = new Mongo.Collection("lap_times");
 
 if (Meteor.isClient) {
-  // This code only runs on the client
   Template.body.helpers({
     currentBoard: function() {
-      return LeaderBoards.findOne({}, {sort: {createdDtm: -1}});
+      return getCurrentLeaderboard();
     },
 
     lapTimes: function () {
-      var leaderboard = LeaderBoards.findOne({}, {sort: {createdDtm: -1}});
-      var cursor = LapTimes.find({leaderboardId: leaderboard.id}, {sort: {time: 1}});
+      var leaderboard = getCurrentLeaderboard();
+      var cursor = LapTimes.find({leaderboard_id: leaderboard._id}, {sort: {time: 1}});
       var drivers = [];
       var result = [];
       cursor.forEach(function(i) {
         if (drivers.indexOf(i.user) == -1) {
-          result.push(i);
+          result.push(
+              {
+                "driver": i.driver,
+                "time": msToTime(i.time)
+              });
           drivers.push(i.user);
         }
       });
@@ -25,33 +28,51 @@ if (Meteor.isClient) {
 
   Template.body.events({
     "submit .new-time": function (event) {
-      // Prevent default browser form submit
       event.preventDefault();
 
-      var leaderboard = LeaderBoards.findOne({}, {sort: {createdDtm: -1}});
+      var leaderboard = getCurrentLeaderboard();
       var time = event.target.time.value;
+      if (/^([0-9]?[0-9]):([0-5][0-9]).([0-9])$/.test(time)) {
+        var timeMs = timeToMs(time);
+        LapTimes.insert({
+          leaderboard_id: leaderboard._id,
+          owner: Meteor.userId(),
+          driver: Meteor.user().username,
+          time: Number(timeMs),
+          created_dtm: new Date()
+        });
+        event.target.time.value = "";
 
-      // Insert a time into the collection
-      LapTimes.insert({
-        leaderboardId: leaderboard.id,
-        owner: Meteor.userId(),
-        user: Meteor.user().username,
-        time: time,
-        createdAt: new Date() // current time
-      });
-
-      // Clear form
-      event.target.time.value = "";
+      } else {
+        alert ('Nice try. Lap times must be formatted as follows: 00:00.0');
+      }
     }
   });
 
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
-}
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  function getCurrentLeaderboard() {
+    return LeaderBoards.findOne({}, {sort: {created_dtm: -1}});
+  }
+
+  function timeToMs(time) {
+    var minutes = Number(time.split(':')[0]);
+    var seconds = Number(time.split(':')[1].split('.')[0]);
+    var ms = Number(time.split('.')[1]);
+
+    return (minutes * 60 * 1000) + (seconds * 1000) + (ms * 100);
+  }
+
+  function msToTime(ms) {
+    var milliseconds = parseInt((ms % 1000) / 100);
+    var seconds = parseInt((ms / 1000) % 60);
+    var minutes = parseInt((ms / (1000 * 60)) % 60);
+
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return minutes + ":" + seconds + "." + milliseconds;
+  }
 }
