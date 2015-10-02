4c9c3994f52dcabc68db79aab2aff4e43039c1a0
andrewf <andrewf@porch.com>
2015-10-02 14:55:54 -0700

    More robust hipchat messaging

--- lap-times/lap-times.js
+++ lap-times/lap-times.js
@@ -108,15 +108,20 @@ if (Meteor.isClient) {
   function notifyHipChat(leaderboard, time, timeMs) {
     var driver = Meteor.user().username;
     var personalBest = LapTimes.findOne({driver: driver, leaderboard_id: leaderboard}, {sort: {time: 1}});
-    var personalBestTime = personalBest ? personalBest.time : 9999999;
-    var faster = timeMs < personalBestTime;
-    var overallBest = LapTimes.findOne({leaderboard_id: leaderboard}, {sort: {time: 1}});
-    var overallBestTime = overallBest ? overallBest.time : 9999999;
-    var fastest = timeMs < overallBestTime;
-    var color = faster ? "green" : "red";
+    var personalBestMs = personalBest ? personalBest.time : 9999999;
+    var faster = timeMs < personalBestMs;
+    var diff = Math.abs((timeMs - personalBestMs) / 1000);
+    var close = diff * 1000 / personalBestMs <= .01;
+
+    var color = faster ? "green" : close ? "yellow" : "red";
     var message1 = driver + " has completed a lap in " + time + ".";
-    var message2 = faster ? " Nice work!" : " Better luck next time.";
-    var message3 = fastest ? " That's a new record!!" : "";
+    var message2 = faster ? " Nice work! That's a " + diff + " second improvement!" : close ? " Not too shabby." : " Better luck next time.";
+
+    var overallBest = LapTimes.findOne({leaderboard_id: leaderboard}, {sort: {time: 1}});
+    var overallBestMs = overallBest ? overallBest.time : 9999999;
+    var message3 = timeMs < overallBestMs ? " And it's also a new all-time record!" : "";
+
+    alert(color + " : " +message1 + message2 + message3);
 
     var xhttp = new XMLHttpRequest();
     xhttp.open("POST", "https://api.hipchat.com/v2/room/Racing/notification?auth_token=Mk8MPiX1IuDD9ujFlIWkN2F12dXHflpIR1Nx1Koj", true);
